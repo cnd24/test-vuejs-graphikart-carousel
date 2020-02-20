d8c5d38efbf65fbc7c278e463d638a9e7df6bf6c
Cindy Planchenault <cnd.planchenault@gmail.com>
2020-02-20 15:07:04 +0100

    initialisation des composants

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
     <div>
+        <carousel>
+            <carousel-slide>
+                Salut les gens
+            </carousel-slide>
+            <carousel-slide>
+                Aurevoir les gens
+            </carousel-slide>
+        </carousel>
         <todos></todos>
     </div>
 </template>
 
 <script>
     import Todos from './components/Todos.vue'
+    import Carousel from './components/carousel/Carousel.vue'
+    import CarouselSlide from "./components/carousel/CarouselSlide";
 
     export default {
         name: 'App',
         components: {
-            Todos
+            Todos,
+            Carousel,
+            CarouselSlide
         }
     }
 </script>
@@ -19,3 +31,4 @@
 
 
 </style>
+
---
+++ src/components/carousel/Carousel.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        data (){
+            return {
+                index: 0,
+                slides: [],
+            }
+        },
+        mounted() {
+            this.slides = this.$children
+            this.slides.forEach((slide, i) => {
+                slide.index = i
+            })
+        }
+    }
+</script>
